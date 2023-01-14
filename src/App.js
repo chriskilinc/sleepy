68e0d389a2b8509f034e777ac2f837d29a19ac8e
chriskilinc <chriskilinc@gmail.com>
2023-01-14 16:50:11 +0100

    Add calculate bedtime/wakeuptime switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 import dayjs from 'dayjs';
-
 import logo from './assets/sleeping.svg';
 import './App.css';
-
 import { Controls } from './components/controls/controls';
 import { TimeItems } from './components/time-items/time-items';
 import { LocalizedTimePicker } from './components/time-picker/time-picker';
@@ -13,6 +11,7 @@ function App() {
   const [selectedTime, setSelectedTime] = useState(dayjs(new Date()));
   const [fallASleepTime, setfallASleepTime] = useState(15);   //  should be customizable 
   const [sleepCycles, setSleepCycles] = useState(6);
+  const [calculatingBedtime, setCalculatingBedtime] = useState(false);
 
   const calculateWakeTime = (bedtime) => {
     const cycleTime = 90;
@@ -24,12 +23,12 @@ function App() {
     return timeItems;
   }
 
-  const onCurrentBedtime = (e) => {
-    setTimeItems(calculateWakeTime(new Date()).reverse());
+  const onSelectedCalculation = () => {
+    calculatingBedtime ? console.log("TODO") : setTimeItems(calculateWakeTime(selectedTime).reverse());
   }
 
-  const onSelectedBedtime = (e) => {
-    setTimeItems(calculateWakeTime(selectedTime).reverse());
+  const onCurrentBedtime = (e) => {
+    setTimeItems(calculateWakeTime(new Date()).reverse());
   }
 
   const onReset = (e) => {
@@ -45,7 +44,7 @@ function App() {
           {(timeItems && timeItems.length === 0) && <LocalizedTimePicker value={selectedTime} setValue={setSelectedTime} />}
           <TimeItems timeItems={timeItems} />
           <Controls timeItems={timeItems} selectedTime={selectedTime}
-            onCurrentBedtime={onCurrentBedtime} onSelectedBedtime={onSelectedBedtime} onReset={onReset} />
+            onSelectedCalculation={onSelectedCalculation} onCurrentBedtime={onCurrentBedtime} onReset={onReset} checked={calculatingBedtime} setChecked={setCalculatingBedtime} />
         </div>
       </main>
     </div>
--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -1,12 +1,17 @@
 import dayjs from 'dayjs';
 import './controls.css';
+import { CustomizedSwitches } from '../switch/switch';
 
 export const Controls = (props) => {
     return (
         <section className='controls' >
+            <div className="control" visible={(props.timeItems.length === 0).toString()}>
+                <CustomizedSwitches label={props.checked ? "calculate suggested bedtimes" : "calculate suggested wakeup-times"} checked={props.checked} setChecked={props.setChecked} />
+            </div>
+
             <div className='control' visible={(props.timeItems.length === 0).toString()}>
-                <p>if i go to bed at: <b>{dayjs(props.selectedTime).format("HH:mm")}</b></p>
-                <button className='btn' onClick={props.onSelectedBedtime}>calculate selected wake-up time</button>
+                <p>{props.checked ? "if i want to wake up at" : "if i go to bed at"} <b>{dayjs(props.selectedTime).format("HH:mm")}</b></p>
+                <button className='btn' onClick={props.onSelectedCalculation}>calculate when to {props.checked ? "go to bed" : "wake up"}</button>
             </div>
 
             {/* TODO: add seperator here? */}
