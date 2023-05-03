09dac1339d3a45cf6d0ec36264e7d7788028835c
chriskilinc <chriskilinc@gmail.com>
2023-05-03 22:52:02 +0200

    switch place of buttons

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -4,13 +4,13 @@ export const Controls = (props) => {
     return (
         <section className='controls' >
             <div className='control segmented' visible={(props.timeItems.length === 0).toString()}>
-                <button className='btn' onClick={props.onWakeUp}>
-                    <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5v-1m0 13.99v-.998m6-5.997h1m-13 0h-1m2-4.996l-1-1m12 0l-1 1m-10 9.993l-1 1m12 0l-1-1m-2-4.997a2.999 2.999 0 01-3 2.998 2.999 2.999 0 113-2.998z" stroke="currentColor" strokeLinecap="square"></path></svg>
-                    Wake up
-                </button>
                 <button className='btn' onClick={props.onGoToBed}>
                     <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M1.66 11.362A6.5 6.5 0 007.693.502a7 7 0 11-6.031 10.86z" stroke="currentColor" strokeLinejoin="round"></path></svg>
                     Go to bed
+                </button>
+                <button className='btn' onClick={props.onWakeUp}>
+                    <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M7.5 1.5v-1m0 13.99v-.998m6-5.997h1m-13 0h-1m2-4.996l-1-1m12 0l-1 1m-10 9.993l-1 1m12 0l-1-1m-2-4.997a2.999 2.999 0 01-3 2.998 2.999 2.999 0 113-2.998z" stroke="currentColor" strokeLinecap="square"></path></svg>
+                    Wake up
                 </button>
             </div>
 
