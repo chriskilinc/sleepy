ba5009f8c0a52483745d0ada53ce7b1925499871
Christopher Kilinc <chriskilinc@gmail.com>
2023-09-21 23:22:24 +0200

    Small changes to pwa functionallity

--- src/index.js
+++ src/index.js
@@ -35,3 +35,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+  }).catch(function (error) {
+    console.log('ServiceWorker registration failed:', error);
+  });
+}
